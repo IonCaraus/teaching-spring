851605e3c57927b3d420e54787745c270b5dc274
ion.caraus <ion.caraus@globant.com>
2024-09-11 09:44:55 +0300

    spring boot, in memory database

--- src/main/java/my/demo/AppConfig.java
+++ src/main/java/my/demo/AppConfig.java
@@ -1,12 +1,14 @@
 package my.demo;
 
 import my.demo.repositories.PersonFileRepository;
+import my.demo.repositories.PersonInMemoryDBRepository;
 import my.demo.repositories.PersonInMemoryRepository;
 import my.demo.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
@@ -23,5 +25,12 @@ public class AppConfig {
     @Qualifier("personRepository")
     public PersonRepository personFileRepository(){
         return new PersonFileRepository();
+    }
+
+    @Profile("qa")
+    @Bean
+    @Qualifier("personRepository")
+    public PersonRepository personInMemoryDBRepository(JdbcTemplate jdbcTemplate){
+        return new PersonInMemoryDBRepository(jdbcTemplate);
     }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table PERSONS(
+                      ID int not null AUTO_INCREMENT,
+                      FIRST_NAME varchar(100) not null,
+                      LAST_NAME varchar(100) not null,
+                      PERSON_NUMBER varchar(100) not null,
+                      EMAIL_ADDRESS varchar(100) not null,
+                      PRIMARY KEY ( ID )
+);
